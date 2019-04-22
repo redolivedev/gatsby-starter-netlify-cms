ad4b92f2c470a00792e27d2b8c98465a4e41862c
Michael Bonner <michael@redolive.com>
2019-04-22 10:26:44 -0600

    add red olive to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,6 +35,11 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
+                      <Link className="navbar-item" to="/red-olive">
+                        Red Olive
+                      </Link>
+                    </li>
+                    <li>
                       <Link className="navbar-item" to="/products">
                         Products
                       </Link>
